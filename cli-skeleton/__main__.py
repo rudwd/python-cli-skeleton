71a78c59b18eb0a68b347189f39c90f7a61c5ce0
HJ Kim <kode.hjkim@gmail.com>
2024-06-25 11:55:56 +0100

    Example 'tail' command.

--- cli-skeleton/__main__.py
+++ cli-skeleton/__main__.py
@@ -1,5 +1,6 @@
 import click
 from pathlib import Path
+from collections import deque
 
 
 @click.command('ls')
@@ -13,7 +14,7 @@ from pathlib import Path
 
                 ))
 @click.version_option('0.0.2', prog_name='skeleton cli')
-def cli(paths):
+def ls(paths):
     for i, path in enumerate(paths):
         if len(paths) > 1:
             click.echo(f'PATH: {path}/')
@@ -27,5 +28,17 @@ def cli(paths):
             click.echo()
 
 
+@click.command()
+@click.option('-n', '--lines', type=click.INT, default=10)
+@click.argument(
+    'file',
+    type=click.File(mode='r')
+)
+def tail(file, lines):
+    for line in deque(file, maxlen=lines):
+        click.echo(line, nl=False)
+
+
 if __name__ == '__main__':
-    cli()
+    # ls()
+    tail()
